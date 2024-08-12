0df70cd94414727ad0b3f26a05437e12dc0d9f0a
Damon <504075864@qq.com>
2024-08-12 10:11:57 +0800

    chore: Update .gitignore file to exclude database files

---
+++ Infrastructure/Utility/JsonHelper.cs
@@ -0,0 +1,29 @@
+﻿namespace MDP.Infrastructure.Utility;
+
+using System.Text.Encodings.Web;
+using System.Text.Json;
+
+/// <summary>
+/// Json帮助类
+/// </summary>
+public class JsonHelper
+{
+  // 写入默认格式
+  public static readonly JsonSerializerOptions s_writeOptions = new()
+  {
+    PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower, // 驼峰命名
+    Converters = { new FmDateTimeConverter() },// 自定义日期时间转换器
+    Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping // 禁止uni编码
+  };
+
+  public static string Serialize<T>(T value)
+  {
+    return JsonSerializer.Serialize(value, s_writeOptions);
+  }
+
+  public static T? Deserialize<T>(string json)
+  {
+    return JsonSerializer.Deserialize<T>(json, s_writeOptions);
+  }
+}
+
